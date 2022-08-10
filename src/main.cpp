e793ef4ce51de498e89320ff913201245e9e765d
Angelo Petrai <agjelo.petraj10@gmail.com>
2022-08-10 15:12:53 +0300

    Add character E.
    Signed-off-by: Angelo Petrai <agjelo.petraj10@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,24 @@
 #include <sstream>
 #include <algorithm>
 #include <vector>
-#include <cassert>
-#include <exception>
 
-static std::map<std::string, std::string> C__ {{".-", "A"}, {"-...", "B"},
-                                               {"-.-.", "C"}, {"-..", "D"}};
+
+static std::map<std::string, std::string> C__ {{".-", "A"}};
 static std::map<std::string, std::string> T__{{"A", ".-"}, {"B", "-..."},
-                                              {"C", "-.-."}, {"D", "-.."}};
+                                              {"C", "-.-."}, {"D", "-.."} };
 
 
 int main(int argc, char **argv) {
     std::string buffer;
     
-    std::getline(std::cin, buffer);
+    for(int i = 0; i != 1; i++)
+      std::cin >> buffer;
 
     for(auto it = buffer.begin(); it != buffer.end(); ++it) 
     {
-      std::string buff(1, *it);
-
-      auto res = T__.find(buff);   
-      std::cout << res->second << " ";
-      if(buff == " ")
-        std::cout << "/ ";
-      
+      std::string buff(1, *it);   
+      std::cout << T__[buff] << " ";
     }
     
     return 0;
-  }
+}
