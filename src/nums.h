602fd5911d2aeeeb237585062fb534e244e5dc4a
Angelo Petrai <agjelo.petraj10@gmail.com>
2022-08-18 16:56:45 +0300

    Include nums.h.
    Signed-off-by: Angelo Petrai <agjelo.petraj10@gmail.com>

---
+++ src/nums.h
@@ -0,0 +1,12 @@
+#ifndef NUMS_H
+#define NUMS_H
+
+#include <map>
+#include <string>
+
+
+static inline std::map<std::string, std::string> N__{};
+static inline std::map<std::string, std::string> N_C{};
+
+
+#endif
