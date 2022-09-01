160eb146a567f267e92a6f84f09384b5bcfae58e
Angelo Petrai <agjelo.petraj10@gmail.com>
2022-09-01 13:50:47 +0300

    Add main functionality.
    -Decode or encode letters or characters
    - Recognize slashes or spaces.
    
    Signed-off-by: Angelo Petrai <agjelo.petraj10@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <sstream>
 #include <algorithm>
 #include <vector>
+#include <string>
+#include <cctype>
+#include <cassert>
+#include <iterator>
 
 #include "dictionary.h"
 #include "nums.h"
@@ -11,14 +15,32 @@
 int main(int argc, char **argv) {
     std::string buffer;
     
-    for(int i = 0; i != 1; i++)
-      std::cin >> buffer;
+    std::getline(std::cin, buffer);
+    std::istringstream C__w(buffer);
+    for(auto &it : buffer) 
+    { 
+      std::string B_chars;
+      C__w >> B_chars;
+      
+      std::string B_letter(1, it);
 
-    for(auto it = buffer.begin(); it != buffer.end(); ++it) 
-    {
-      std::string buff(1, *it);   
-      std::cout << T__[buff] << " ";
+      auto C_Buff = std::find(B_chars.begin(), B_chars.end(), '.');
+      auto C__Buff = std::find(B_chars.begin(), B_chars.end(), '-');
+
+      if(C_Buff != B_chars.end() || C__Buff != B_chars.end()) 
+           std::cout << C__[B_chars];
+      else 
+           std::cout << T__[B_letter] << " ";
+        
+
+        if(B_chars == "/") 
+         std::cout << " ";
+        else if(B_letter == " ")
+         std::cout << "/ ";
+       
+      B_chars.clear();
+      B_letter.clear();
     }
-    
+    printf("\n");
     return 0;
 }
